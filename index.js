a46c1a1b7f6eb5749e791314150acddf974cb8e3
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-01-18 11:53:34 +0500

    welcome to JavaScript

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("Welcome to JavaScript!");
+
+const data = new Date();
+
+console.log(data);
+
+//Javascript display possibilities;
+
+//(1) console.log("Umar");
+
+//(2) alert("Umar");
+
+//(3)const name = document.getElementById("name");
+//name.innerHTML = "Umar";
+
+//(4) document.write("Umar");
+
+//A program is a list of programming statements;
+
+let x, y, z; //statement 1;
+
+x = 5; //statement 2;
+y = 6;
+z = x + y;
+
+console.log(z);
+
+//Javascript Syntax;
+//syntax is the set of rules for writing programs in a language;
+
+let a;
+let b;
+let c;
+a = 5;
+b = 6;
+c = a + b;
+
+console.log(c);
